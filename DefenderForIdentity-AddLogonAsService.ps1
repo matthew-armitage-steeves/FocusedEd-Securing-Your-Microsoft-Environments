33833d67ed2240ee28016f02b215a94bf9c601c3
Matthew Armitage <114957313+matthew-armitage-steeves@users.noreply.github.com>
2024-10-24 14:10:30 -0700

    Fixed Script

--- DefenderForIdentity-AddLogonAsService.ps1
+++ DefenderForIdentity-AddLogonAsService.ps1
@@ -1,5 +1,5 @@
 ## Requires AD RSAT tools to be installed
 
 install-module psPrivilege
-$gMSAuser = (Get-AdUser -Identity DfIDirAccess)
+$gMSAuser = (Get-ADServiceAccount -Identity DfIDirAccess)
 Add-WindowsRight -Name SeServiceLogonRight -Account $gMSAuser.sid
